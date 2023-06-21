9398f07fb6c95d5026240539c597e477ed34fd6b
sakthi_l_u_c_i <sakthivel8725@outlook.com>
2023-06-21 12:20:52 +0530

    feat: local storage done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TaskForms from "./components/TaskForms";
 import Task from "./components/Task";
 function App() {
   const [tasks, setTasks] = useState([]);
-  console.log(tasks);
+
+  useEffect(() => {
+    if (tasks.length === 0) {
+      return;
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+    setTasks(storedTasks);
+  }, []);
+
   function addTask(name) {
-    console.log("task", name);
     setTasks((prev) => [...prev, { name: name, done: false }]);
   }
   return (
     <>
       <main>
         <TaskForms onAdd={addTask} />
-
         {tasks.map((task) => (
           <Task key={task.id} {...task} />
         ))}
